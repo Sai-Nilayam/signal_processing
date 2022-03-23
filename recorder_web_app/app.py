5114fc0d80ad593c368a35cf6f2edf8a1c462a72
Sai-Nilayam <sainilayamsahu777@gmail.com>
2022-03-23 20:25:26 +0530

    Recorder web app on progress.

--- recorder_web_app/app.py
+++ recorder_web_app/app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request
+import json
+import librosa
+import numpy as np
+import soundfile as sf
+import os
 
 app = Flask(__name__)
 
@@ -14,9 +19,187 @@ def test():
 @app.route('/system_1/get_unique_words', methods=['POST'])
 def get_unique_words():
 	voice = request.form.get("voice")
-	all_texts = request.form.get("voice")
-	return str(voice)
+	all_texts = request.form.get("all_texts")
 
+	# Now we have the voice and the all_texts.
+	# Now here we need to do the processigs.
+
+	# First we are going to clean the data i.e to remove all the punctuations 
+	# and convert all the characters to lowercase.
+	symbol_list = [
+	    '`', '~', '!', '@', '$', '%', '^', '&', '*', '(', ')', '-', '=',
+	    '[', '{', ']', '}', ';', ':', "'", '"', ',', '<', '>', '.', '/', '?', 
+	    '\\', '|', '\n'
+	]
+
+	for symbol in symbol_list:
+		all_texts = all_texts.replace(symbol, ' ')
+
+	all_texts = all_texts.lower()
+
+	# Now that we have all the cleaned texts, now we need to find all the
+	# unique words.
+	all_words = all_texts.split(' ')
+	unique_words = set(all_words)
+	unique_words_list = list(unique_words)
+	
+	# Now we need to save it to a text file in the form of a list but in text 
+	# form so that we could execute these all when needed. 
+	unique_words_str = ''
+	for word in unique_words_list:
+		unique_words_str = unique_words_str + ' ' + word
+	unique_words_str = unique_words_str.strip()
+
+	f = open('static/system_1/{}/unique_words.txt'.format(voice), 'w')
+	f.write(unique_words_str)
+	f.close()
+
+	# Now we need to prepare set of data that is needed to be returned.
+	unique_word_count = len(unique_words_list)
+	
+	if unique_word_count%8 == 0:
+		no_of_takes = unique_word_count // 8 - 1
+	else:
+		no_of_takes = (unique_word_count//8)
+
+	estimated_recording_time = no_of_takes * 0.5
+
+	# Preparing the json object
+	response = {
+		'unique_word_count': str(unique_word_count),
+		'no_of_takes': str(no_of_takes),
+		'estimated_recording_time': str(estimated_recording_time)
+	}
+
+	response_json = json.dumps(response)
+
+	return response_json
+
+# Now it's time to create an url to get take words. 
+@app.route('/system_1/get_take_words', methods=['POST'])
+def get_take_words():
+	voice = request.form.get('voice')
+	take_no = request.form.get('take_no')
+
+	range_a = int(take_no) * 8 
+	range_b = range_a + 8
+
+	# Now we need to get all the unique words from the unique_words.txt file and
+	# make a list out of that.
+	f = open('static/system_1/{}/unique_words.txt'.format(voice), 'r')
+	content = f.read()
+	f.close()
+
+	unique_words_list = content.split(' ')
+
+	take_words_list = unique_words_list[range_a : range_b]
+
+	take_words_str = ' '.join(take_words_list)
+
+	# Preparing the json object
+	response = {
+		'take_words': take_words_str
+	}
+
+	response_json = json.dumps(response)
+
+	print(response_json)
+
+	return response_json
+
+# Here is the url to process the audio clip comming from the front end split it
+# into word chunks and return all the audio chunks.
+@app.route('/system_1/process_audio', methods=['POST'])
+def process_audio():
+	voice = request.form.get('voice')
+	words = request.form.get('words')
+	crop_amp_threshold = request.form.get('crop_amp_threshold')
+	audio = request.files['audio']
+	
+	# Now that we have all the data we require now it's time to process the 
+	# audio file, split it into 8 different words. If the split array count is
+	# 8 then there will be further steps done. Else, Just return a response 
+	# the Response that the audio processing failed. If success then returns
+	# would be 8 audio chunk files and all the chunks would be previously saved 
+	# in within inst folder of system_1.
+
+	# First take the audio file and save it to the inst_words folder.
+	audio.save('static/system_1/{}/audio_take_inst.mp3'.format(voice))
+
+	# Now the next thing is that we would be importing that back to python 
+	# environment and spit it using the librosa codes.
+	ar, sr = librosa.load('static/system_1/{}/audio_take_inst.mp3'.format(voice))
+
+	# Now it's time to split it.
+	intervals = librosa.effects.split(ar, top_db=int(crop_amp_threshold))
+
+	# Send failed message if the no of cropped words are not queals to 8.
+	if len(intervals) != 8:
+		# For internal testing.
+		print(intervals)
+
+		# We need to return a json here.
+		# Preparing the json object
+		response = {
+			'processing_status': 'failed'
+		}
+
+		response_json = json.dumps(response)
+
+		return response_json
+
+	# Now that we have intervals we need to crop the audio files and save them
+	# in the inst_word folder. 
+	# But before that we need to have access to the take words.
+	take_words_arr = words.split(' ')
+
+	for i in range(len(intervals)):
+	    inst_interval = intervals[i]
+	    inst_audio = ar[inst_interval[0] : inst_interval[1]]
+	    inst_file_name = take_words_arr[i]
+	    # Only use this when you are saving audio clips.
+	    sf.write('static/system_1/{}/inst_words/{}.wav'.format(voice, i), inst_audio, sr, 'PCM_24')
+
+	# Now what are the things we need to return. 
+	# All we need to return a success message.
+	# Preparing the json object
+	response = {
+		'processing_status': 'success'
+	}
+
+	response_json = json.dumps(response)
+
+	return response_json
+
+# Now we need to write a function to grab all the word clips from the inst_words
+# folder and save them to the words folder with all the proper names. Wav format
+# is okay to be used as in our case the wav files are very small as result of 
+# being converted from the mp3 files by changing extensions.
+@app.route('/system_1/done', methods=['POST'])
+def done():
+	voice = request.form.get('voice')
+	words = request.form.get('words')
+
+	# Now here the first thing is to get all the files from the inst_words 
+	# folder.
+	files = os.listdir('static/system_1/{}/inst_words'.format(voice))
+	files.sort()
+
+	# Create the words array
+	words_arr = words.split(' ')
+
+	for i in range(len(files)):
+		inst_file_name = files[i]
+		ar, sr = librosa.load('static/system_1/{}/inst_words/{}'.format(voice, inst_file_name))
+
+		# Getting the real file name from the word arr.
+		real_file_name = words_arr[i]
+		sf.write('static/system_1/{}/words/{}.wav'.format(voice, real_file_name), ar, sr, 'PCM_24')
+
+		# Removing the file from the inst_words folder.
+		os.remove('static/system_1/{}/inst_words/{}'.format(voice, inst_file_name))
+
+	return 'done'
 
 if __name__ == '__main__':
 	app.run(debug=True, port=8000)
--- recorder_web_app/static/index.js
+++ recorder_web_app/static/index.js
@@ -12,7 +12,13 @@ function analyse() {
 
  	xhttp = new XMLHttpRequest();
  	xhttp.onload = function() {
-		document.getElementById("unique_word_count").innerHTML = this.responseText;       
+		response_text = this.responseText;
+		response_json = JSON.parse(response_text);
+
+		document.getElementById("unique_word_count").innerHTML = response_json.unique_word_count;
+		document.getElementById("no_of_takes").innerHTML = response_json.no_of_takes;
+		document.getElementById("estimated_recording_time").innerHTML = response_json.estimated_recording_time; 
+		document.getElementById("total_no_of_takes").innerHTML = response_json.no_of_takes;  
 	}
 
  	xhttp.open("POST", url);
@@ -25,7 +31,270 @@ function analyse() {
 	form_data.append('all_texts', all_texts)
 
 	xhttp.send(form_data)
+}
 
- 	// alert(all_texts);
+// Now we need to write a funciton to get the texts for each takes.
+function get_take_words() {
+	url = '/system_1/get_take_words'
+
+ 	xhttp = new XMLHttpRequest();
+ 	xhttp.onload = function() {
+		response_text = this.responseText;
+		response_json = JSON.parse(response_text);
+		
+		document.getElementById("take_words").innerHTML = response_json.take_words; 
+	}
+
+ 	xhttp.open("POST", url);
+ 	// Preparing the data
+ 	voice = document.getElementById("select_voice").value;
+ 	take_no = document.getElementById("take_no").value;
+ 	
+ 	form_data = new FormData()
+ 	form_data.append('voice', voice)
+ 	form_data.append('take_no', take_no)
+
+	xhttp.send(form_data)
+}
+
+// Now we need to add some functionality for the start recording button.
+function start_rec() {
+	// alert('test')
+	document.getElementById("rec_animation").style.visibility = "visible";
+	document.getElementById("start_rec").style.display = "none";
+	document.getElementById("stop_rec").style.display = "inline-block";
+	document.getElementById("stop_rec").style.visibility = "visible"
+
+	// Starting the recording process.
+	navigator.mediaDevices.getUserMedia({ audio: true })
+		.then(stream => {
+		    mediaRecorder = new MediaRecorder(stream);
+
+		    // Starts the audio recording proecess.
+		    mediaRecorder.start();
+
+		    // Saving the audio data in chunks while recording.
+		    const audioChunks = [];
+
+		    mediaRecorder.addEventListener("dataavailable", event => {
+		      audioChunks.push(event.data);
+		    });
+
+		    // Converting the mdedia recorder object to a blob object with the event listener stop
+		    // and then creating an audio url for that blob.
+		    mediaRecorder.addEventListener("stop", () => {
+		      audioBlob = new Blob(audioChunks, {type: "audio/ogg"});
+		      // audioUrl = URL.createObjectURL(audioBlob);
+		    });
+
+	  	});
+}
+
+// Now we need to write some functionality for stop recording button.
+// This function is very sophisticated to be used.
+function stop_rec() {
+	// Stopping the Media Recorder.
+ 	mediaRecorder.stop()
+
+	// Hiding the entire recording components.
+	document.getElementById("stop_rec").style.visibility = "hidden";
+	document.getElementById("rec_animation").style.visibility = "hidden";
+	document.getElementById("please_wait").style.visibility = "visible";
+
+	// Playing the Audio. Before doing anything to the audio, we need to set a Time out so that 
+	// it would get some time to do some sort of pre-processing and making ready the audio file.
+	// setTimeout(() => {
+ //     	audio = new Audio(audioUrl);
+ //    	audio.play();
+ //    }, 200);
+
+ 	// We don't actually need to play the audio here. Instead we need to send the audio file 
+ 	// to the server. Here are the codes to send the audio file to the server.
+ 	url = '/system_1/process_audio'
+
+ 	xhttp = new XMLHttpRequest();
+ 	xhttp.onload = function() {
+		response_text = this.responseText;
+		response_json = JSON.parse(response_text);
+		// alert(response_json.processing_status)
+		if (response_json.processing_status == "failed") {
+			document.getElementById("please_wait").style.visibility = "hidden";
+			document.getElementById("processing_status").innerHTML = "Processing failed. Please try out with different Cropping Amp. Thresholds or use a more noise less microphone.";
+			document.getElementById("processing_status").style.color = "#ff4f42";
+			document.getElementById("processing_status").style.visibility = "visible";
+			document.getElementById("post_operations").style.visibility = "visible";
+		};
+
+		if (response_json.processing_status == "success") {
+			document.getElementById("please_wait").style.visibility = "hidden";
+			document.getElementById("processing_status").innerHTML = "Success . . !";
+			document.getElementById("processing_status").style.color = "#00c76a";
+			document.getElementById("processing_status").style.visibility = "visible";
+			document.getElementById("post_operations").style.visibility = "visible";
+			document.getElementById("word_clips").style.visibility = "visible";
+		}
+
+	}
+
+ 	xhttp.open("POST", url);
+ 	// Preparing the data
+ 	voice = document.getElementById("select_voice").value;
+ 	words = document.getElementById("take_words").innerHTML;
+ 	crop_amp_threshold = document.getElementById("crop_amp_threshold").value;
+
+ 	form_data = new FormData()
+
+ 	form_data.append('voice', voice);
+ 	form_data.append('words', words);
+ 	form_data.append('crop_amp_threshold', crop_amp_threshold);
+
+ 	setTimeout(() => {
+ 		// Creating an audio file from audio blog. This takes time so in the time_out scope.
+     	audio_file = new File([audioBlob], "audio.mp3"); 
+    }, 200);
+
+    setTimeout(() => {
+	 	// Appending the file object to the form data after being created. The creation takes some time. 
+		form_data.append('audio', audio_file);
+		// Sending the form data after every required elelment is added to the form data.
+		xhttp.send(form_data)
+    }, 400);
+ 		
+}
+
+// Now we need to write a funciton for retake.
+function retake() {
+	document.getElementById("post_operations").style.visibility = "hidden";
+	document.getElementById("word_clips").style.visibility = "hidden";
+	document.getElementById("processing_status").style.visibility = "hidden";
+	document.getElementById("stop_rec").style.display = "none";
+	document.getElementById("start_rec").style.display = "inline-block";
+	document.getElementById("start_rec").style.visibility = "visible";
+}
+
+// Now here writing a bunch of functions to play the clip audios.
+function clip_0() {
+	clip_url = "http://127.0.0.1:8000/static/system_1/voice_0_0/inst_words/0.wav"
+ 	audio = new Audio(clip_url);
+
+ 	audio.onended = function() {
+ 		document.getElementById("clip_0_animation").style.visibility = "hidden";
+ 	}
+
+ 	document.getElementById("clip_0_animation").style.visibility = "visible";
+	audio.play()	
+}
+
+function clip_1() {
+	clip_url = "http://127.0.0.1:8000/static/system_1/voice_0_0/inst_words/1.wav"
+ 	audio = new Audio(clip_url);
+
+ 	audio.onended = function() {
+ 		document.getElementById("clip_1_animation").style.visibility = "hidden";
+ 	}
+
+ 	document.getElementById("clip_1_animation").style.visibility = "visible";
+	audio.play()	
+}
+
+function clip_2() {
+	clip_url = "http://127.0.0.1:8000/static/system_1/voice_0_0/inst_words/2.wav"
+ 	audio = new Audio(clip_url);
+
+ 	audio.onended = function() {
+ 		document.getElementById("clip_2_animation").style.visibility = "hidden";
+ 	}
+
+ 	document.getElementById("clip_2_animation").style.visibility = "visible";
+	audio.play()	
+}
+
+function clip_3() {
+	clip_url = "http://127.0.0.1:8000/static/system_1/voice_0_0/inst_words/3.wav"
+ 	audio = new Audio(clip_url);
+
+ 	audio.onended = function() {
+ 		document.getElementById("clip_3_animation").style.visibility = "hidden";
+ 	}
+
+ 	document.getElementById("clip_3_animation").style.visibility = "visible";
+	audio.play()	
+}
+
+function clip_4() {
+	clip_url = "http://127.0.0.1:8000/static/system_1/voice_0_0/inst_words/4.wav"
+ 	audio = new Audio(clip_url);
+
+ 	audio.onended = function() {
+ 		document.getElementById("clip_4_animation").style.visibility = "hidden";
+ 	}
+
+ 	document.getElementById("clip_4_animation").style.visibility = "visible";
+	audio.play()	
+}
+
+function clip_5() {
+	clip_url = "http://127.0.0.1:8000/static/system_1/voice_0_0/inst_words/5.wav"
+ 	audio = new Audio(clip_url);
+
+ 	audio.onended = function() {
+ 		document.getElementById("clip_5_animation").style.visibility = "hidden";
+ 	}
+
+ 	document.getElementById("clip_5_animation").style.visibility = "visible";
+	audio.play()	
+}
+
+function clip_6() {
+	clip_url = "http://127.0.0.1:8000/static/system_1/voice_0_0/inst_words/6.wav"
+ 	audio = new Audio(clip_url);
+
+ 	audio.onended = function() {
+ 		document.getElementById("clip_6_animation").style.visibility = "hidden";
+ 	}
+
+ 	document.getElementById("clip_6_animation").style.visibility = "visible";
+	audio.play()	
+}
+
+function clip_7() {
+	clip_url = "http://127.0.0.1:8000/static/system_1/voice_0_0/inst_words/7.wav"
+ 	audio = new Audio(clip_url);
+
+ 	audio.onended = function() {
+ 		document.getElementById("clip_7_animation").style.visibility = "hidden";
+ 	}
+
+ 	document.getElementById("clip_7_animation").style.visibility = "visible";
+	audio.play()	
+}
+
+// Now writing the function for done.
+function done() {
+	url = '/system_1/done'
+
+ 	xhttp = new XMLHttpRequest();
+ 	xhttp.onload = function() {
+		// response_text = this.responseText;
+		// response_json = JSON.parse(response_text);
+
+		document.getElementById("retake").click();
+		current_take_no = document.getElementById("take_no").value;
+		current_take_no = parseInt(current_take_no)
+		document.getElementById("take_no").value = current_take_no + 1;
+		document.getElementById("get_take_words").click();
+
+	}
+
+ 	xhttp.open("POST", url);
+ 	// Preparing the data
+ 	voice = document.getElementById("select_voice").value;
+ 	words = document.getElementById("take_words").innerHTML;
+ 	
+ 	form_data = new FormData()
+ 	form_data.append('voice', voice)
+ 	form_data.append('words', words)
+
+	xhttp.send(form_data)
 }
 
